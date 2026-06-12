88c18eecc5bab9e9cdd384b042310daf853ef8e9
Ali Hammoud <letsmailali.h@gmail.com>
2026-06-12 07:42:25 +0300

    feat: add prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,34 +1,16 @@
-// import eslint from '@eslint/js';
-// import eslintPluginAstro from 'eslint-plugin-astro';
-// import tseslint from 'typescript-eslint';
-
-// export default [
-//   eslint.configs.recommended,
-//   ...tseslint.configs.recommended,
-//   ...eslintPluginAstro.configs.recommended,
-//   {
-//     ignores: ['dist/', '.astro/', 'node_modules/', 'pnpm-lock.yaml'],
-//   },
-//   {
-//     files: ['**/*.{mjs,js,cjs}'],
-//     languageOptions: {
-//       globals: {
-//         process: 'readonly',
-//       },
-//     },
-//   },
-// ];
-
-import { defineConfig } from "eslint/config";
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import astro from "eslint-plugin-astro";
-import astroParser from "astro-eslint-parser";
+import js from "@eslint/js"
+import astroParser from "astro-eslint-parser"
+import prettierConfig from "eslint-config-prettier"
+import astro from "eslint-plugin-astro"
+import { defineConfig } from "eslint/config"
+import tseslint from "typescript-eslint"
 
 export default defineConfig([
   js.configs.recommended,
   ...tseslint.configs.recommended,
   ...astro.configs.recommended,
+
+  prettierConfig,
   {
     ignores: ["dist/", ".astro/", "node_modules/", "pnpm-lock.yaml"],
   },
@@ -51,4 +33,4 @@ export default defineConfig([
       },
     },
   },
-]);
+])
